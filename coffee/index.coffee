50d34b452872539344aa7998d8dc9d6bef310a47
KamataRyo <mugil.cephalus+github@gmail.com>
2015-09-03 20:29:32 +0900

    test commit

--- coffee/index.coffee
+++ coffee/index.coffee
@@ -1,19 +1,41 @@
-p3 = d3
-	.select 'body'
-	.selectAll 'p'
 
-
-dataset = [12, 24, 32]
-p3
- .data dataset
- .text (d, i) ->
-	 return "#{i + 1}番目は#{d}"
 # googlemapの初期設定
-# $map = $('#map')
-# initialPosition = new google.maps.LatLng 35.127152, 138.910627
-# myOptions =
-# 	noClear : true,
-# 	center : initialPosition,
-# 	zoom : 16,
-# 	mapTypeId : google.maps.MapTypeId.ROADMAP
-# googlemapCanvas = new google.maps.Map $map[0], myOptions# $map[0]でdomに変換
+mapElement = document.getElementById 'map-canvas'
+initialPosition = new google.maps.LatLng 35.127152, 138.910627
+myOptions =
+	noClear : true,
+	center : initialPosition,
+	zoom : 16,
+	mapTypeId : google.maps.MapTypeId.ROADMAP
+map = new google.maps.Map mapElement, myOptions
+
+
+
+
+d3.json './NPs.json', (data) ->
+	console.log data
+	styles = data.styles
+
+	d3.json './geojson/NPS_fujihakoneizu.geojson', (data) ->
+
+		styleFeature = (feature) ->
+
+			determineStyle = (feature) ->
+				description = feature.getProperty('description')
+				for style in styles
+					if description.match ///#{style.name}///
+						return style
+				return 'else'
+			style = determineStyle feature
+
+			styleForMap =
+				fillColor: style.fill
+				strokeColor: style.stroke
+				strokeWeight: 1
+				fillOpacity: 0.2
+			return styleForMap
+
+
+
+		map.data.addGeoJson data
+		map.data.setStyle styleFeature
--- js/index.js
+++ js/index.js
@@ -1,9 +1,47 @@
-var dataset, p3;
+var initialPosition, map, mapElement, myOptions;
 
-p3 = d3.select('body').selectAll('p');
+mapElement = document.getElementById('map-canvas');
 
-dataset = [12, 24, 32];
+initialPosition = new google.maps.LatLng(35.127152, 138.910627);
 
-p3.data(dataset).text(function(d, i) {
-  return (i + 1) + "番目は" + d;
+myOptions = {
+  noClear: true,
+  center: initialPosition,
+  zoom: 16,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+map = new google.maps.Map(mapElement, myOptions);
+
+d3.json('./NPs.json', function(data) {
+  var styles;
+  console.log(data);
+  styles = data.styles;
+  return d3.json('./geojson/NPS_fujihakoneizu.geojson', function(data) {
+    var styleFeature;
+    styleFeature = function(feature) {
+      var determineStyle, style, styleForMap;
+      determineStyle = function(feature) {
+        var description, i, len, style;
+        description = feature.getProperty('description');
+        for (i = 0, len = styles.length; i < len; i++) {
+          style = styles[i];
+          if (description.match(RegExp("" + style.name))) {
+            return style;
+          }
+        }
+        return 'else';
+      };
+      style = determineStyle(feature);
+      styleForMap = {
+        fillColor: style.fill,
+        strokeColor: style.stroke,
+        strokeWeight: 1,
+        fillOpacity: 0.2
+      };
+      return styleForMap;
+    };
+    map.data.addGeoJson(data);
+    return map.data.setStyle(styleFeature);
+  });
 });
